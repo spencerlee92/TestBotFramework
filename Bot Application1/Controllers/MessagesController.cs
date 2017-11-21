2243336822a731fb9af3f2fb4bb17aa5e01f0787
Spencer <spencerlee92123@gmail.com>
2017-11-22 12:25:49 +1300

    UpdateMultiDialogScenrio
    This is based on the initial bot skeleton. I followed the insturction on: https://docs.microsoft.com/en-us/bot-framework/dotnet/bot-builder-dotnet-manage-conversation-flow

--- Bot Application1/Controllers/MessagesController.cs
+++ Bot Application1/Controllers/MessagesController.cs
@@ -7,6 +7,51 @@ using Microsoft.Bot.Connector;
 
 namespace Bot_Application1
 {
+    [System.Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        protected int count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            if (message.Text == "reset")
+            {
+                PromptDialog.Confirm(
+                    context,
+                    AfterResetAsync,
+                    "Are you sure you want to reset the count?",
+                    "Didn't get that!",
+                    promptStyle: PromptStyle.None);
+            }
+            else
+            {
+                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+
     [BotAuthentication]
     public class MessagesController : ApiController
     {
--- Bot Application1/Dialogs/RootDialog.cs
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -8,24 +8,66 @@ namespace Bot_Application1.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
-        {
-            context.Wait(MessageReceivedAsync);
 
-            return Task.CompletedTask;
+        private string name;
+        private int age;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            /* Wait until the first message is received from the conversation and call MessageReceviedAsync 
+             *  to process that message. */
+            context.Wait(this.MessageReceivedAsync);
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-            var activity = await result as Activity;
+            /* When MessageReceivedAsync is called, it's passed an IAwaitable<IMessageActivity>. To get the message,
+             *  await the result. */
+            var message = await result;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            await this.SendWelcomeMessageAsync(context);
+        }
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+        private async Task SendWelcomeMessageAsync(IDialogContext context)
+        {
+            await context.PostAsync("Hi, I'm the Basic Multi Dialog bot. Let's get started.");
 
-            context.Wait(MessageReceivedAsync);
+            context.Call(new NameDialog(), this.NameDialogResumeAfter);
+        }
+
+        private async Task NameDialogResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                this.name = await result;
+
+                context.Call(new AgeDialog(this.name), this.AgeDialogResumeAfter);
+            }
+            catch (TooManyAttemptsException)
+            {
+                await context.PostAsync("I'm sorry, I'm having issues understanding you. Let's try again.");
+
+                await this.SendWelcomeMessageAsync(context);
+            }
+        }
+
+        private async Task AgeDialogResumeAfter(IDialogContext context, IAwaitable<int> result)
+        {
+            try
+            {
+                this.age = await result;
+
+                await context.PostAsync($"Your name is { name } and your age is { age }.");
+
+            }
+            catch (TooManyAttemptsException)
+            {
+                await context.PostAsync("I'm sorry, I'm having issues understanding you. Let's try again.");
+            }
+            finally
+            {
+                await this.SendWelcomeMessageAsync(context);
+            }
         }
     }
 }
